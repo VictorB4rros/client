47e1ea571f1f42ea097542db5eedf6c1198a2d6a
Victor Barros <rodrigues.barros@aluno.ifsp.edu.br>
2025-07-20 17:49:10 -0300

    find all

--- src/main/java/com/victor/client/services/ClientService.java
+++ src/main/java/com/victor/client/services/ClientService.java
@@ -1,6 +1,8 @@
 package com.victor.client.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,5 +20,11 @@ public class ClientService {
 	public ClientDTO findById(Long id) {
 		Client client = repository.findById(id).get();
 		return new ClientDTO(client);
+	}
+	
+	@Transactional(readOnly = true)
+	public Page<ClientDTO> findAll(Pageable pageable) {
+		Page<Client> result = repository.findAll(pageable);
+		return result.map(x -> new ClientDTO(x));
 	}
 }
